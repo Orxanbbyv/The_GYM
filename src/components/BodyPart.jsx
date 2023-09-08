da1f82953682db537248d473398b447967a7d27f
Orxanbbyv <116581123+Orxanbbyv@users.noreply.github.com>
2023-09-09 00:29:50 +0400

    exercises

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -9,7 +9,7 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
         type="button"
         alignItems="center"
         justifyContent="center"
-        className="bodyPart-card"
+        className="bodyPart-card "
         sx={{
           borderTop: bodyPart === item ? "4px solid #ff2625" : "",
           backgroundColor: "#fff",
@@ -19,12 +19,17 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
           cursor: "pointer",
           gap: "47px",
         }}
+        onClick={()=>{
+          setBodyPart(item);  
+          window.scrollTo({top:1800, left:100, behavior:"smooth"})
+        }}
       >
         <img
           src={Icon}
           alt="dumbbell"
-          styles={{ width: "40px", height: "40px" }}
+          style={{ width: "40px", height: "40px" }}
         />
+        <Typography fontSize="24px" fontWeight="bold" color="#3A1212" textTransform="capitalize">{item}</Typography>
       </Stack>
     </div>
   );
--- src/components/HorizontalScrolbar.jsx
+++ src/components/HorizontalScrolbar.jsx
@@ -1,10 +1,68 @@
 import React from "react";
-import { Box } from "@mui/material";
-import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+import { Box, Typography } from "@mui/material";
+import Slider from "react-slick";
 import BodyPart from "./BodyPart";
+import leftArrowImage from "../assets/icons/left-arrow.png"
+import rightArrowImage from "../assets/icons/right-arrow.png"
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+
 const HorizontalScrolbar = ({ data, bodyPart, setBodyPart }) => {
+
+
+  const CustomNextArrow = (props) => {
+    const { onClick } = props;
+    return (
+      <Typography className="left-arrow" onClick={onClick}>
+        <img src={rightArrowImage} alt="Next" />
+      </Typography>
+    );
+  };
+  
+  const CustomPrevArrow = (props) => {
+    const { onClick } = props;
+    return (
+      <Typography className="right-arrow" onClick={onClick}>
+        <img src={leftArrowImage} alt="Prev" />
+      </Typography>
+    );
+  };
+
+
+  const breakpoints = [
+    { min: 1200, max: 1500, slidesToShow: 4, slidesToScroll: 4 },
+    { min: 992, max: 1199, slidesToShow: 3, slidesToScroll: 3 },
+    { min: 768, max: 991, slidesToShow: 2, slidesToScroll: 2 },
+    { min: 480, max: 767, slidesToShow: 1, slidesToScroll: 1 },
+    
+  ];
+
+  const responsiveSettings = breakpoints.map((bp) => ({
+    breakpoint: bp.max + 1,
+    settings: {
+      slidesToShow: bp.slidesToShow,
+      slidesToScroll: bp.slidesToScroll,
+    },
+  }));
+
+  const sliderSettings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: responsiveSettings,
+    nextArrow: <CustomNextArrow />,
+    prevArrow: <CustomPrevArrow />,
+  };
+
   return (
-    <ScrollMenu>
+
+    <Slider {...sliderSettings}>
       {data.map((item) => (
         <Box
           key={item.id || item}
@@ -15,7 +73,7 @@ const HorizontalScrolbar = ({ data, bodyPart, setBodyPart }) => {
           <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
         </Box>
       ))}
-    </ScrollMenu>
+    </Slider>
   );
 };
 
