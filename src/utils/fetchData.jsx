bb5d3da88552b062db46c83348d8051b4919b8c8
Orxanbbyv <116581123+Orxanbbyv@users.noreply.github.com>
2023-09-10 19:14:13 +0400

    exercisedetail

--- src/utils/fetchData.jsx
+++ src/utils/fetchData.jsx
@@ -1,9 +1,18 @@
-const REACT_APP_RAPID_API_KEY="94bfc49157mshffabd7b83d3fd20p1bb199jsn279568e52658"
+const REACT_APP_RAPID_API_KEY =
+  "94bfc49157mshffabd7b83d3fd20p1bb199jsn279568e52658";
 
 export const exerciseOptions = {
   method: "GET",
   headers: {
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    "X-RapidAPI-Key": REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
     "X-RapidAPI-Key": REACT_APP_RAPID_API_KEY,
   },
 };
